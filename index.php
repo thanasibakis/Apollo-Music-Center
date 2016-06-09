023014ad985f3d9bc807d62be74e36245ee8fa2f
Thanasi Bakis <thanasibakis@yahoo.com>
2016-06-08 19:31:06 -0700

    Index page layout
    Created index page with a header, featured items list, and cart list.
    The header and item pieces are PHP includes.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" href="stylesheets/index.css" type="text/css" media="screen"/>
+		<link rel="stylesheet" href="stylesheets/header.css" type="text/css" media="screen"/>
+		<link rel="stylesheet" href="stylesheets/item.css" type="text/css" media="screen"/>
+		<title>Store</title>
+	</head>
+	<body>
+		<?php include 'html_include/header.html' ?>
+		<section>
+			<h3>Featured</h3>
+			<div class="item_scroll">
+				<table>
+					<tr>
+						<td><?php include 'html_include/item_small.html' ?></td>
+						<td><?php include 'html_include/item_small.html' ?></td>
+						<td><?php include 'html_include/item_small.html' ?></td>
+						<td><?php include 'html_include/item_small.html' ?></td>
+						<td><?php include 'html_include/item_small.html' ?></td>
+					</tr>
+				</table>
+			</div>
+		</section>
+		<section>
+			<h3>Your Cart</h3>
+			<div class="item_scroll">
+				<table>
+					<tr>
+						<td><?php include 'html_include/item_small.html' ?></td>
+						<td><?php include 'html_include/item_small.html' ?></td>
+					</tr>
+				</table>
+			</div>
+		</section>
+	</body>
+</html>
