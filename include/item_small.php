50dda0a6f8e72e07f31e531d7fb7a1634b50ba87
Thanasi Bakis <thanasibakis@yahoo.com>
2016-06-18 14:59:11 -0700

    Item_small fixes again
    Safari mobile needs shorter names to not wrap

--- include/item_small.php
+++ include/item_small.php
@@ -1,6 +1,6 @@
 <a href="item.php?id=<?php echo $id; ?>">
 	<div class="item_small">
-		<h3><?php echo trim(substr($name, 0, 30)); if(strlen($name) > 30){ echo '...'; } ?></h3>
+		<h3><?php echo trim(substr($name, 0, 27)); if(strlen($name) > 27){ echo '...'; } ?></h3>
 		<img src="<?php echo $image; ?>" alt="Hmmm... this should be <?php echo $name; ?>"/>
 		<p><?php echo trim(substr($description, 0, 35)); if(strlen($description) > 35){ echo '...'; } ?></p>
 		<table>
